7efc8a93a399377029d91f5b5ce12323ac3c86e0
Nodirbek <sulaymanovnasim@gmail.com>
2024-08-20 18:40:28 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+        int n, a = 0;
+        Console.Write("n=");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            a = a + i * i - (i - 1) * (i - 1);
+        }
+        Console.WriteLine(a);
+    
